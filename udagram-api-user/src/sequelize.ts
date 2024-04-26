80c16559c0851fdf3e6beabfc1d773286e13f2f9
HideOnHp <hideonhp@gmail.com>
2024-04-26 14:55:05 +0700

    Split api into feed and user

--- udagram-api-user/src/sequelize.ts
+++ udagram-api-user/src/sequelize.ts
@@ -2,20 +2,26 @@ import {Sequelize} from 'sequelize-typescript';
 import {config} from './config/config';
 import { SequelizeConfig } from 'sequelize-typescript/lib/types/SequelizeConfig';
 
-const sequelizeConfig: SequelizeConfig = {
+let sequelizeConfig: SequelizeConfig = {
   'username': config.username,
   'password': config.password,
   'database': config.database,
   'host': config.host,
-  'dialectOptions': {
-    'ssl': {
-      'require': true,
-      'rejectUnauthorized': false,
-    },
-  },
   'dialect': config.dialect,
   'storage': ':memory:',
 };
+
+if(process.env.NODE_ENV === "local") {
+  sequelizeConfig = {
+    ...sequelizeConfig,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    },
+  };
+}
 
 
 export const sequelize = new Sequelize(sequelizeConfig);
