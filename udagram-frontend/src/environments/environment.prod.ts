1d682b2436155ce257321a45ce61fbb435248b35
HideOnHp <hideonhp@gmail.com>
2024-05-02 14:01:52 +0700

    Update for part iv

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a83d0698fd7884e3891b7671c5e8f105-1866544110.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
